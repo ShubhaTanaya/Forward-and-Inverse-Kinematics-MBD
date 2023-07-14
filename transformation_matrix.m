8b83054f05ddbda5d7401ca7f70befc96a45dfe7
ShubhaTanaya <104180527+ShubhaTanaya@users.noreply.github.com>
2023-07-14 17:41:40 +0530

    Add files via upload

---
+++ transformation_matrix.m
@@ -0,0 +1,7 @@
+function ret = transformation_matrix(theta, alpha, d, a)
+
+    ret = [cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha), a*cos(theta);
+           sin(theta), cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
+           0, sin(alpha), cos(alpha), d;
+           0, 0, 0, 1];
+end
